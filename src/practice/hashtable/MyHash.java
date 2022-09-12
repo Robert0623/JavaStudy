d98ff9916fcc7b3794beb11699b7c49084aa576d
HyeonWooSeo <tone8943@gmail.com>
2022-09-12 21:54:56 +0900

    HashTable 구현 - 1

---
+++ src/practice/hashtable/MyHash.java
@@ -0,0 +1,36 @@
+package practice.hashtable;
+
+//해시테이블 구현1
+public class MyHash {
+    public Slot[] hashTable;
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+    public class Slot {
+        String value;
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if(this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if(this.hashTable[address] != null) {
+            return this.hashTable[address].value;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/practice/hashtable/MyHash2.java
@@ -0,0 +1,61 @@
+package practice.hashtable;
+
+//해시테이블 구현2 -
+public class MyHash2 {
+    public Slot[] hashTable;
+
+    public MyHash2(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+    public class Slot {
+        String key;
+        String value;
+        Slot next;
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if(this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while(findSlot != null) {
+                if (findSlot.key == key) {
+                    findSlot.value = value;
+                    return true;
+                } else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value);
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if(this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+}
